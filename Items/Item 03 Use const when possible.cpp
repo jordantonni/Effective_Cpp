8e2f7cc6f53eaec2a9f2a7d35905410ea4f558d6
Jordan Tonni <jordantonni@outlook.com>
2017-01-05 00:26:29 +0000

    Item 3 complete

--- Items/Item 03 Use const when possible.cpp
+++ Items/Item 03 Use const when possible.cpp
@@ -4,6 +4,10 @@
  * Bitwise const - Data memebers of the class arent modified in any way (but data pointed to by memeber pointer could be) How c++ compiler
  *                 enforces const.
  * Logical const - Idea that nothing should be changed though a const memeber method. Including data a memeber pointer points to.
+ * 
+ * Compilers enforce bitwise const, but I should program using logical constnes
+ * 
+ * If const and non const methods have same implementations. Call the const from the non const using casts.
  */
 
 #include <vector>
@@ -40,16 +44,20 @@ namespace item03
     struct textBlock
     {
         std::string text;
+        mutable int text_length; // Mutable allows this field to be modified even in a const method!
 
         // Needs to return a reference otherwise wouldnt be able to write to the actual value in the string, would get a copy instead!
         // Also functions return by value. So you would be assigning to an r-value (illegal) if you wrote to it!
+        // Note how we use the definition of LOGICAL constness, we are changing a memeber field, text_length, but for all intents and purposes, it is a const function.
         const char& operator[](const std::size_t index) const
         {
+            text_length = text.length();
             return text[index];
         }
 
         char& operator[](const std::size_t index)
         {
+            text_length = text.length();
             return text[index];
         }
     };
@@ -67,4 +75,33 @@ namespace item03
     {
         char val = tb[2]; // Can only work if textBlock has a const operator[], due to being passed in as a const ref
     }
+
+    /*
+     * Avoiding code duplication using const casts
+     * 
+     * If you have const and non const versions of methods to ensure both const and non const objects can call them
+     * you can end up with code duplication. Calling the const version from the non const can help.
+     * 
+     * NOTE: Never call the non const from the const (via removing the const with a const_cast) as that would allow 
+     * the objects' internal state to be modified. 
+     */
+
+    class anotherTextBlock
+    {
+        std::string text;
+
+        const char& operator[](const int position) const
+        {
+            return text[position];
+        }
+
+        // Cast itself *this to a const version of itself, call the const operator[] 
+        // remove const and return it
+        char& operator[](const int position)
+        {
+            return const_cast<char&>(
+                static_cast<const anotherTextBlock&>(*this)[position]
+            );
+        }
+    };
 }
