7d00f69cffcc0f8ad27a2498eb3d29219f62a4bf
Jordan Tonni <jordantonni@outlook.com>
2017-01-17 15:00:39 +0000

    Finished item 34

--- Items/Item 34 Differentiate inheritance interface vs implementation.cpp
+++ Items/Item 34 Differentiate inheritance interface vs implementation.cpp
@@ -5,6 +5,23 @@
  *  - Pure Virtual -> Interface only
  *  - Virtual      -> Interface + Default implementation (can specialize and change)
  *  - Non-Virtual  -> Interface + Mandatory implementation (can't change)
+ *  
+ * Seperating functions from interface and implementation - Via splitting default virtual functions
+ *  - Instead of having fly be virtual to provide a default implementation, we make it pure virtual and put a default fly implementation in protected section.
+ *  - Any class that wants the deafult behavior just has to forward to default_fly in their fly override.
+ *  - Can also be done via pure virtual implementation, both shown below
+ *  - Why? 
+ *      - Stops classes from accidently inheritating default behavior they shouldnt support!
+ * 
+ * Non-Virtual functions
+ *  - Specify an INVARIANT over the hierarchy of specializations
+ * 
+ * SUMMARY:
+ *  - Inheritance of interface is different from inheritance of impmentation
+ *  - Public inheritance -> defived classes always inherit base class interface
+ *  - Pure virtual -> interface only
+ *  - Virtual -> interface +  default implementation
+ *  - Non-Virtual -> Interface + mandatory (invariant) implementation
  */
 
 #include <iostream>
@@ -12,44 +29,38 @@ using namespace std;
 
 namespace item34
 {
-    class base
+    class Aeroplane
     {
-    public:
-        virtual void draw() = 0;
-
-        void print()
+    protected:
+        virtual void default_fly()
         {
-            cout << "base" << endl;
+            // do some stuff
         }
+    public:
+        virtual void fly() = 0;
     };
 
-    class d : public base
+    void Aeroplane::fly()
     {
-    public:
-        void draw() override
-        {
-            int x;
-        }
-
-                void print()
-                {
-                    cout << "der" << endl;
-                }
-    };
-
-    void base::draw()
-    {
-        std::cout << "hehe" << std::endl;
+        // Could also add a default implementation to the pure virtual function here. 
     }
+
+
+    /*
+     * Derived class specifies it wants default implementation, instead of just getting it automatically 
+     */
+    class SpecialPlane : Aeroplane
+    {
+    public:
+        virtual void fly()
+        {
+            default_fly(); 
+
+            Aeroplane::fly(); // Another way to ask specifically for default behaviour using a pure virtual implementation!
+        }
+    };
 
 
     void test()
-    {
-        d x;
-        base* bp = &x;
-        d* dp = &x;
-
-        bp->print();
-        dp->print();
-    }
+    { }
 }
