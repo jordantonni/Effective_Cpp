36844651dbf00410473f254e4d7caa7f4bb5ddcb
Jordan Tonni <jordantonni@outlook.com>
2017-01-19 18:51:19 +0000

    End of project

---
+++ Items/Summary.cpp
@@ -0,0 +1,11 @@
+/*
+ * Don't ignore compiler warnings without first understanding what exactly it is warning you about.
+ * 
+ * C++98 Standard Library:
+ *  - Standard Template Library (STL): Containers, Iterators, Algorithims, Function Objects, Container Adapters
+ *  - Iostreams: Buffering, Internationalized IO, Predefined IO objects (cin, cout, ceer, clog)
+ *  - Internatioanlization: wchar etc
+ *  - Numeric Processing: templates for complex numbers and arrays of pure values (valarray)
+ *  - Exception Hierarchy: Base Exception, Derived Logic_error and runtime_error, everything else derives from them
+ *  - C89's Std Library: Everything in C89 C library
+ */
